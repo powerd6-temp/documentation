513b25dc2088536726b282c8606601fa756216ca
Hector Castelli Zacharias <hector.zacharias@gmail.com>
2023-09-12 21:03:23 +0200

    document glossary related contents

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,17 +1,23 @@
 import { z, reference, defineCollection } from 'astro:content';
 
+// Defines a term and it's meaning
 const glossary = defineCollection({
     type: 'data',
     schema: z.object({
-        word: z.string(),
+        // The term, or word
+        term: z.string(),
+        // A short definitions
         definition: z.string(),
+        // Optionally, a reference to a page that further explains it
         relatedPage: reference('glossaryPage').optional(),
     }),
 });
 
+// Explains a specific glossary item at length
 const glossaryPage = defineCollection({
     type: 'content',
     schema: z.object({
+        // The relevant glossary term
         glossary: reference('glossary')
     })
 })
