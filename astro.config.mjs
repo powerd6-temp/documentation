63c45678e80c29cf5572ea1e62f0293f64ffd30f
Hector Castelli Zacharias <hector.zacharias@gmail.com>
2023-09-19 20:31:41 +0200

    add alpine

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
-
+import alpinejs from "@astrojs/alpinejs";
 import mdx from "@astrojs/mdx";
 import pagefind from "astro-pagefind";
 import tailwind from "@astrojs/tailwind";
 
+
 // https://astro.build/config
 export default defineConfig({
   build: {
-    format: "file",
+    format: "file"
   },
-  integrations: [mdx(), pagefind(), tailwind()]
+  integrations: [mdx(), pagefind(), tailwind(), alpinejs()]
 });
